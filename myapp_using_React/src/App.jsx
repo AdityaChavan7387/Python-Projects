13a3cf1b0ff3ad4a76a5f907d3aec2cccafced85
Aditya Chavan <adityachavan7387@gmail.com>
2026-01-17 17:10:11 +0530

    Added new project using API

---
+++ myapp_using_React/src/App.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./component/Welcome"; 
+import Greeting from "./component/Greeting";
+function App() {
+  return (
+    <div>
+      <Greeting name="Alice" />
+    </div>
+  );
+}
+export default App;
