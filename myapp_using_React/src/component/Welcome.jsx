9eafd02d6f9e51df8e6bb50d7d83788f8bea5665
Aditya Chavan <adityachavan7387@gmail.com>
2026-01-17 17:09:43 +0530

    Added new project using API

---
+++ myapp_using_React/src/component/Welcome.jsx
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <div>
+      <h1>Welcome to Our Application!</h1>
+      <p>We're glad to have you here. Explore and enjoy your stay!</p>
+    </div>
+  );
+}
+export default Welcome;
